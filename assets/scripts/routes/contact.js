5980c91c1d0ff02d847e44782d0610c6c72229e8
Nate Beaty <nate@clixel.com>
2020-07-06 14:13:44 -0400

    refactor contact form to work within Craft framework, adjust form submit handling to handle errors and display success status after submit

--- assets/scripts/routes/contact.js
+++ assets/scripts/routes/contact.js
@@ -10,47 +10,67 @@ const contact = {
     document.querySelector('.contact-form-wrap button[type=submit]').addEventListener('mouseover', e => {
       e.target.textContent = 'Let’s Go';
     });
-    document.querySelector('.contact-form-wrap button[type=submit]').addEventListener('focus', e => {
-      e.target.textContent = 'Let’s Go';
-    });
     document.querySelector('.contact-form-wrap button[type=submit]').addEventListener('mouseout', e => {
       e.target.textContent = 'Submit';
+    });
+    // Focus/blur support
+    document.querySelector('.contact-form-wrap button[type=submit]').addEventListener('focus', e => {
+      e.target.textContent = 'Let’s Go';
     });
     document.querySelector('.contact-form-wrap button[type=submit]').addEventListener('blur', e => {
       e.target.textContent = 'Submit';
     });
 
-    // Contact message autoexpands
-    let contactText = document.querySelector('#contact-form textarea');
-    if (contactText) {
-      autosize(contactText);
-    }
-
     // AJAXify contact form submission
-    let form = $('#contact-form');
+    let $form = $('#contact-form');
     let formResponse = document.querySelector('.contact-form-wrap .form-response');
     let formWrap = document.querySelector('.contact-form-wrap .form-wrap');
-    form.submit(function(e) {
+
+    // Store initial thanks copy if replaced by error
+    let thanksHtml = formResponse.innerHTML;
+
+    $form.on('submit', (e) => {
       e.preventDefault();
-      $.ajax({
-        url: form.attr('action'),
-        type: form.attr('method'),
-        dataType: 'html',
-        data: form.serialize(),
-        success: (result) => {
-          console.log(result);
-          formResponse.innerHTML = result;
-          if (result.match('success')) {
+      if (appState.requestInProgress) {
+        return false;
+      }
+      let $subject = $form.find('input[name=subject]');
+      let fromName = $form.find('input[name=fromName]').val();
+
+      // Set dynamic subject line to avoid gmail making a conversation thread and hiding text
+      $subject.val($subject.data('original-value') + ' from ' + fromName);
+
+      // Set appState flag to avoid multiple submits
+      appState.requestInProgress = true;
+
+      // Submit form
+      $.post({
+        url: '/',
+        dataType: 'json',
+        data: $form.serialize(),
+        success: (response) => {
+          if (response.success) {
+            formResponse.innerHTML = thanksHtml;
             formWrap.classList.add('-success');
+            zenscroll.center(formResponse);
+          } else {
+            formResponse.innerHTML = response.error || '<p>There was an error, please try again.</p>';
+            zenscroll.center(formResponse);
           }
-          zenscroll.center(formResponse);
         },
-        error: (result) => {
-          formResponse.innerHTML = '<p>There was an error, please try again.</p>';
+        error: (response) => {
+          formResponse.innerHTML = response.error || '<p>There was an error, please try again.</p>';
           zenscroll.center(formResponse);
         }
+      }).fail((response) => {
+        formResponse.innerHTML = response.error || '<p>There was an error, please try again.</p>';
+        zenscroll.center(formResponse);
+      }).always(() => {
+        formResponse.classList.add('-active');
+        appState.requestInProgress = false;
       });
     });
+
   },
 
   finalize() {
